f8a75b97ddb0bd5d9876f2a91eab5d7d465b78c6
Ana <anavitoriamurad@usp.br>
2025-01-18 17:44:29 -0300

    datapath

--- experiment_03/Verilog/circuito_exp3.v
+++ experiment_03/Verilog/circuito_exp3.v
@@ -24,54 +24,54 @@ module circuito_exp3 (
 
     // Fluxo de Dados
     exp3_fluxo_dados FD (
-        .clock(clock)
-        .chaves(chaves)
-        .zeraR(s_zeraR) // zera registradores
-        .registraR(s_registraR) // 
-        .contaC(s_contaC) // incrementa contagem
-        .zeraC(s_zeraC) // zera contagem
-        .chavesIgualMemoria(db_igual)
-        .fimC(s_fimC) // fim da contagem
-        .db_contagem(s_contagem)
-        .db_chaves(s_chaves)
-        .db_memoria(s_memoria)
+        .clock              ( clock       ),
+        .chaves             ( chaves      ),
+        .zeraR              ( s_zeraR     ), // zera registradores
+        .registraR          ( s_registraR ), // registra registradores
+        .contaC             ( s_contaC    ), // incrementa contagem
+        .zeraC              ( s_zeraC     ), // zera contagem
+        .chavesIgualMemoria ( db_igual    ),
+        .fimC               ( s_fimC      ), // fim da contagem
+        .db_contagem        ( s_contagem  ),
+        .db_chaves          ( s_chaves    ),
+        .db_memoria         ( s_memoria   )
     );
 
     // Unidade de Controle
     exp3_unidade_controle UC (
-        .clock(clock)
-        .reset(reset)
-        .iniciar(iniciar)
-        .fimC(s_fimC)
-        .zeraC(s_zeraC)
-        .contaC(s_contaC)
-        .zeraR(s_zeraR)
-        .registraR(s_registraR)
-        .pronto(pronto)
-        .db_estado(s_estado)
+        .clock     ( clock       ),
+        .reset     ( reset       ),
+        .iniciar   ( iniciar     ),
+        .fimC      ( s_fimC      ),
+        .zeraC     ( s_zeraC     ),
+        .contaC    ( s_contaC    ),
+        .zeraR     ( s_zeraR     ),
+        .registraR ( s_registraR ),
+        .pronto    ( pronto      ),
+        .db_estado ( s_estado    )
     );
 
     // Display das chaves
     hexa7seg HEX2 (
-        .hexa(s_chaves)
-        .display(db_chaves)
+        .hexa    ( s_chaves  ),
+        .display ( db_chaves )
     );
 
     // Display dos endereços codificados
     hexa7seg HEX0 (
-        .hexa(s_contagem)
-        .display(db_contagem)
+        .hexa    ( s_contagem ),
+        .display ( db_contagem )
     );
 
     // Display do conteúdo em uma posição de memória
     hexa7seg HEX1 (
-        .hexa(s_memoria)
-        .display(db_memoria)
+        .hexa    ( s_memoria  ),
+        .display ( db_memoria )
     );
 
     // Display do estado atual
     hexa7seg HEX5 (
-        .hexa(s_estado)
-        .display(db_estado)
+        .hexa    ( s_estado ),
+        .display ( db_estado )
     );
 endmodule
