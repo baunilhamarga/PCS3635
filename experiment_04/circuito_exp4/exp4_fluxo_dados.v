081c33b31124e1bb5b120f41ebf5f6eef56be82f
Ana <anavitoriamurad@usp.br>
2025-01-25 14:56:42 -0300

    ✨ new module exp4_fluxo_dados
    Co-authored-by: baunilhamarga <baunilhamarga@users.noreply.github.com>
    Co-authored-by: Yasmin Francisquetti <yasminfrancisquetti@users.noreply.github.com>

--- experiment_04/circuito_exp4/exp4_fluxo_dados.v
+++ experiment_04/circuito_exp4/exp4_fluxo_dados.v
@@ -31,49 +31,57 @@ module exp4_fluxo_dados (
     wire [3:0] s_endereco;
     wire [3:0] s_dado;
     wire [3:0] s_chaves;
+    wire tem_jogada = |s_chaves;
 
     // contador_163
-    contador_163 contador (
-        .clock(clock),
-        .clr  (~zeraC),
-        .ld   (1'b1),
-        .ent  (1'b1),
-        .enp  (contaC),
-        .D    (4'b0),
-        .Q    (s_endereco),
-        .rco  (fimC)
+    contador_163 contadorJ (
+        .clock (clock),
+        .clr   (~zeraC),
+        .ld    (1'b1),
+        .ent   (1'b1),
+        .enp   (contaC),
+        .D     (4'b0),
+        .Q     (s_endereco),
+        .rco   (fimC)
     );
 
     // comparador_85
     comparador_85 comparador (
-        .A   (s_dado),
-        .B   (s_chaves),
-        .ALBi(1'b0),
-        .AGBi(1'b0),
-        .AEBi(1'b1),
-        .ALBo(),
-        .AGBo(),
-        .AEBo(chavesIgualMemoria)
+        .A    (s_dado),
+        .B    (s_chaves),
+        .ALBi (1'b0),
+        .AGBi (1'b0),
+        .AEBi (1'b1),
+        .ALBo (),
+        .AGBo (),
+        .AEBo (igual)
     );
 
     // memória
     sync_rom_16x4 memoria (
-        .clock   (clock),
-        .address (s_endereco),
-        .data_out(s_dado)
+        .clock    (clock),
+        .address  (s_endereco),
+        .data_out (s_dado)
     );
 
     // registrador
-    registrador_4 registrador (
+    registrador_4 registradorJ (
+        .clock  (clock),
+        .clear  (zeraR),
+        .enable (registraR),
+        .D      (chaves),
+        .Q      (s_chaves)
+    );
+
+    edge_detector detector (
         .clock (clock),
-        .clear (zeraR),
-        .enable(registraR),
-        .D     (chaves),
-        .Q     (s_chaves)
+        .reset (zeraC),
+        .sinal (tem_jogada),
+        .pulso (jogada_feita)
     );
 
     assign db_memoria  = s_dado;
-    assign db_chaves   = s_chaves;
+    assign db_jogada   = s_chaves;
     assign db_contagem = s_endereco;
-
+    assign db_tem_jogada = tem_jogada;
 endmodule
